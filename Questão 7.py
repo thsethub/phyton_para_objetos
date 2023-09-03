f79c43c60edb4a0c1f8ff9659b5a511ad4f2a709
thsethub <thsetbuh@gmail.com>
2023-09-03 17:04:33 -0300

    Phyton para objetos

---
+++ Questão 7.py
@@ -0,0 +1,9 @@
+#entradas
+print('Bem vindo(a) a ferramenta de conversão de metros para centímetro!')
+metros = float(input('Quantos metros você quer converter?\n'))
+
+#processamento
+conversor = metros*100
+
+#imprimindo valores
+print(f'O valor de {metros} metros em centímetros é: {conversor} centímetros.')
